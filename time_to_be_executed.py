39884ed52bc9aebd85cc95fdcb79d9b68d90e141
Lucas Fonseca Merino <lucasfmerino@gmail.com>
2022-10-23 02:58:03 -0300

    inicial commit

---
+++ time_to_be_executed.py
@@ -0,0 +1,24 @@
+from time import time
+
+
+def f_master(func):
+    def time_tbe(*args, **kwargs):
+        star_time = time()
+        result = func(*args, **kwargs)
+        end_time = time()
+        total = (end_time - star_time) * 1000
+        print(f'The function {func.__name__} took {total:.2f} ms to be executed')
+        return result
+
+    return time_tbe
+
+
+@f_master
+def f_printr(int_range):
+    for i in range(int_range):
+        print(i)
+
+
+if __name__ == '__main__':
+
+    f_printr(1000000)
